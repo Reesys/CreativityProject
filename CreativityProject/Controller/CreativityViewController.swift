8af3f5bdcd088ab789422cebb2d5e2b80287277e
Reesys <emailjoshuareyes@gmail.com>
2018-11-01 11:58:22 -0600

    Updated the ViewController to fit with the project.

---
+++ CreativityProject/Controller/CreativityViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CreativityProject
+//
+//  Created by Reyes, Joshua on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class CreativityViewController: UICreativityController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
